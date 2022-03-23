429db3b0e2dd31bd6b1c0da4820556c2ace047af
Micael Gallego <micael.gallego@gmail.com>
2022-03-23 16:56:25 +0100

    Add local IP to ejemplo-1-python

--- docker/ejemplo-1-python/app.py
+++ docker/ejemplo-1-python/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 import random
+import socket
 
 app = Flask(__name__)
 
@@ -18,7 +19,12 @@ images = [
 @app.route('/')
 def index():
     url = random.choice(images)
-    return render_template('index.html', url=url)
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    ip = s.getsockname()[0]
+    s.close()
+
+    return render_template('index.html', url=url, ip=ip)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
